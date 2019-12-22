ea2ef1dd591c09036f3bbf2db608d46bb87fe8c3
Linus Kreitner <linus.kreitner@tum.de>
2019-12-22 13:32:36 +0100

    refactor: simplify routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    children: [
+      {
+        path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule'
+      },
+      {
+        path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule'
+      },
+      {
+        path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule'
+      },
+      {
+        path: '',
+        redirectTo: '/tab1',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 @NgModule({
