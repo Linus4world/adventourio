e2600b8d38094918540a62ec7083461739575dd5
Linus Kreitner <linus.kreitner@tum.de>
2020-01-05 13:29:18 +0100

    refactor(openlayer): set correct gps projection

--- frontend/src/app/components/openlayer/openlayer.component.ts
+++ frontend/src/app/components/openlayer/openlayer.component.ts
@@ -1,10 +1,13 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { defaults as defaultControls } from 'ol/control';
+// Solution based on https://stackoverflow.com/a/48303261
+
+import { Component, AfterViewInit, Input } from '@angular/core';
+// import { defaults as defaultControls } from 'ol/control';
+// import ZoomToExtent from 'ol/control/ZoomToExtent';
 import Map from 'ol/Map';
 import View from 'ol/View';
+import {fromLonLat} from 'ol/proj';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
-import ZoomToExtent from 'ol/control/ZoomToExtent';
 
 
 @Component({
@@ -13,10 +16,20 @@ import ZoomToExtent from 'ol/control/ZoomToExtent';
   styleUrls: ['./openlayer.component.scss'],
 })
 export class OpenlayerComponent implements AfterViewInit {
+  /**
+   * The coordinates of the point of the center of the map. The coordinates are given in EPSG:3857 format (latitude, longitude).
+   * Note that the map later uses long, lat not lat, long
+   */
+  @Input() center: {lat: number, long: number} = {lat: 48.135124, long: 11.581981};
+  /**
+   * The factor to which the map zooms. The user can later change this in the view
+   */
+  @Input() zoom = 13;
 
   map: Map;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngAfterViewInit() {
     this.map = new Map({
@@ -29,17 +42,10 @@ export class OpenlayerComponent implements AfterViewInit {
         })
       ],
       view: new View({
-        center: [813079.7791264898, 5929220.284081122],
-        zoom: 7
+        projection: 'EPSG:3857',
+        center: fromLonLat([this.center.long, this.center.lat]),
+        zoom: this.zoom
       }),
-      controls: defaultControls().extend([
-        new ZoomToExtent({
-          extent: [
-            813079.7791264898, 5929220.284081122,
-            848966.9639063801, 5936863.986909639
-          ]
-        })
-      ])
     });
   }
 
