b5b16222c80a46d858d8128c0364388a32b3ccde
Esteban VP <ge56rum@tum.de>
2020-01-07 00:00:09 +0100

    New file
    Added world_architecture file, to better be able to split the work

--- backend/story_architecture.py
+++ backend/story_architecture.py
@@ -2,21 +2,6 @@ import numpy as np
 import random
 from random_word import RandomWords
 
-
-class Player:
-    def __init__(self):
-        self.name = ''
-        self.feeling = ''
-        self.position = 'ground'
-        self.won = False
-        #self.solves = 0
-
-
-class Location:
-    def __init__(self):
-        pass
-
-
 class Blank:
     def __init__(self, name, words):
         self.name = name
