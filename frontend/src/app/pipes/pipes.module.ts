30412aeb61935dcd9354eda27fdabe17627cb179
Linus Kreitner <linus.kreitner@tum.de>
2020-01-06 19:56:17 +0100

    feat: add account pipe

---
+++ frontend/src/app/pipes/pipes.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AccountLoaderPipe } from './account-loader.pipe';
+
+@NgModule({
+declarations: [AccountLoaderPipe],
+imports: [],
+exports: [AccountLoaderPipe],
+})
+
+export class PipesModule {}
