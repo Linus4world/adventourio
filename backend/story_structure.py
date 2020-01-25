21a838871917c71fb4c615a47a204aa81235bf3d
Esteban VP <ge56rum@tum.de>
2020-01-25 13:56:23 +0100

    reestructuring stuff!

--- backend/story_structure.py
+++ backend/story_structure.py
@@ -70,101 +70,6 @@ class Story:
     def add_challenge_text_input(self, challenge_key, challenge_text):
         pass
 
-class Chapter: #stage
-    def __init__(self, scenarios):
-        self.end = False
-        self.beginning = False
-        self.scenarios = scenarios
-        self.scenario_probabilities = [0] * len(scenarios)
-
-    def get_next_scenario(self, user_inputs):
-        next_scenario = Scenario()
-        if user_inputs is not None:
-            for user_input in user_inputs.values():
-                # Multiple Choice
-                if user_input.q_type == 'multiple choice':
-                    for c, scenario in enumerate(self.scenarios):
-                        scenario_picked_by_user = user_input.scenario_mapping[user_input.answer]
-                        if scenario_picked_by_user == scenario.name:
-                            self.scenario_probabilities[c] += user_input.weight
-
-            next_scenario = self.scenarios[np.argmax(self.scenario_probabilities)]
-
-        else:
-            next_scenario = random.choice(self.scenarios)
-        return next_scenario
-
-
-class Scenario: #substage
-    def __init__(self):
-        self.name = ''
-        self.location = ''
-        self.plot = []  # array of stings!
-        self.blanks = dict()
-        self.user_inputs = dict()
-
-    # This function can later used to add blanks from a dictionary or Wikipedia!
-    def add_blank(self, blank_key, name='', w_type='', words=None, random_word=False):
-        self.blanks[blank_key] = Blank(name=name, words=words, w_type=w_type, random_word=random_word)
-
-    def add_user_input(self, input_key='', name='', q_type='', scenario_mapping=None, weight=0, answer=''):
-        self.user_inputs[input_key] = UserInput(name=name, q_type=q_type, scenario_mapping=scenario_mapping, weight=weight,
-                                                answer=answer)
-
-    def store_user_answer(self, input_key, answer):
-        self.user_inputs[input_key].answer = answer
-
-    # Replace all blank.name for a word from the blank.words list in the plot
-    def fill_in_the_blank(self, blank_key):
-        word = ''
-        if blank_key in self.blanks.keys():
-            if self.blanks[blank_key].random_word:
-                word = get_random_word(self.blanks[blank_key].w_type)
-            else:
-                word = random.choice(self.blanks[blank_key].words)
-        return word
-
-#option choice is dependent on: random number, if players succeeded in LAST task, with what probability the outcomes of the previous stages were selected
-#option choice returns a PROBABILITY to select a good/bad outcome in substage3
-    def determine_substage3(challenge_outcome, prob_array): #challenge outcome is 0 for "failed task" and 1 for "achieved task"
-    #prob_array is a list of tuples like [(0.82, "good"), (0.63, "bad")]
-        total_val = 0;
-        rand_num = round(random.uniform(-1, 1),2);
-        good_probability = max(min(challenge_outcome + rand_num, 1), 0); #all calculations performed for probability to get good event
-        for el in prob_array:
-          if el[1] == "bad":
-              val = -1 * el[0];
-          else:
-              val = el[0];
-          total_val = total_val + val;
-        good_probability = max(min(good_probability + total_val, 1), 0);
-        if (good_probability >= 0.5):
-            prob_array.append((good_probability, "good"));
-        else:
-            prob:array.append((1-good_probability, "bad")); #gets probability for bad event
-        return prob_array;
-
-    def run_scenario(self):
-
-        for txt in self.plot:
-            if len(txt) == 2:
-                # Input
-                if txt[0] == 'I' and txt[1].isnumeric():
-                    input_key = txt
-                    answer = input("\n> ")
-                    self.store_user_answer(input_key=input_key, answer=answer)
-                # Blank
-                elif txt[0] == 'B' and txt[1].isnumeric():
-                    blank_key = txt
-                    blank = self.fill_in_the_blank(blank_key)
-                    print(blank, end='')
-                # Answer: Gives back a user input in the text
-                elif txt[0] == 'A' and txt[1].isnumeric():
-                    input_key = 'I' + txt[1]
-                    user_answer = self.user_inputs[input_key].answer
-                    print(user_answer, end='')
-            else:
-                print(txt, end='')
     def add_stage(self, stage):
         self.stages.append(stage)
 
@@ -300,7 +205,6 @@ class SubStage:
     #                 print(user_answer, end='')
     #         else:
     #             print(txt, end='')
->>>>>>> TASK-01
 
 
 if __name__ == "__main__":
