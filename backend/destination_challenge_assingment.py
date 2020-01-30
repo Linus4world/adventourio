3cc68516f60307f327c4ac5dd2aa3c2e18e6f04b
ge69bum <ge69bum@mytum.de>
2020-01-30 23:09:15 +0100

    changing the key word for consistency

--- backend/destination_challenge_assingment.py
+++ backend/destination_challenge_assingment.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Player:
     # destination - the places of the current stage, player assumed to be there
     # stage - current stage in the story
@@ -9,36 +10,50 @@ class Player:
 
 
 def new_place(player1, player2, places_in):
-
-    destination1 = player1.destination
-    destination2 = player2.destination
+    destination1 = player1.geo_location
+    destination2 = player2.geo_location
 
     # calculate middle point between two players
     if destination1[0] >= destination2[0]:
-        new_latt = destination1[0]-(destination1[0] - destination2[0])/2
+        new_latt = destination1[0] - (destination1[0] - destination2[0]) / 2
     else:
-        new_latt = destination2[0] - (destination2[0] - destination1[0])/2
+        new_latt = destination2[0] - (destination2[0] - destination1[0]) / 2
 
     if destination1[1] >= destination2[1]:
-        new_long = destination1[1] - (destination1[1] - destination2[1])/2
+        new_long = destination1[1] - (destination1[1] - destination2[1]) / 2
     else:
-        new_long = destination2[1] - (destination2[1] - destination1[1])/2
+        new_long = destination2[1] - (destination2[1] - destination1[1]) / 2
 
-    print(places_in)
     r_old = 0
-    place_name = "null"
-    place_id = 0
+    destinationCoords = [0, 0]
+    destinationName = 0
+    challenge = "null"
+    challenge_type = 0
+    options = []
+    right_answer = []
     for stages in places_in['places']:
         if stages['stage'] == (player1.stage + 1) and stages['stage'] == (player2.stage + 1):
             for place in stages['place']:
                 r1 = math.pow(place['coordinates'][0] - new_latt, 2)
                 r2 = math.pow(place['coordinates'][1] - new_long, 2)
-                r = math.sqrt(r1+r2)
+                r = math.sqrt(r1 + r2)
                 if r < r_old or r_old == 0:
-                    place_name = place['title']
-                    place_id = place['id']
+                    destinationName = place['title']
+                    destinationCoords[0] = place['coordinates'][0]
+                    destinationCoords[1] = place['coordinates'][1]
+                    challenge = place['challenge']
+                    challenge_type = place['challenge_type']
+                    if challenge_type == 1:
+                        options = place['options']
+                    right_answer = place['right_answer']
                     r_old = r
+    if challenge_type == 1:
+        one_json = {'destinationName': destinationName, 'destinationCoords': destinationCoords,
+                    'challenge': {'challenge': challenge, 'challenge_type': challenge_type,
+                                  'right_answer': right_answer, 'options': options}}
+    else:
+        one_json = {'destinationName': destinationName, 'destinationCoords': destinationCoords,
+                    'challenge': {'challenge': challenge, 'challenge_type': challenge_type,
+                                  'right_answer': right_answer}}
 
-    return place_name, place_id
-
-
+    return one_json
