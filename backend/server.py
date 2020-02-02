59ec9d9f5a8cd43365f414e3d83c0c1cd2902ac5
Linus Kreitner <linus.kreitner@tum.de>
2020-02-02 14:30:53 +0100

    [B]refactor(server.py): bundle player requests

--- backend/server.py
+++ backend/server.py
@@ -37,47 +37,48 @@ def join(id):
     if session.isFull():
         return abort('Session is full!')
     session.addPlayer(id, answers["name"], answers["answers"])
-    if session.isFull():
+    if session.isReady():
         all_answers = all_answers_function(session)
         with open("challenges.json", 'r', encoding='utf-8') as file:
             places = file.read()
         session.setPlacesCategory(placesCategory(all_answers, places))
-        # TODO
         session.setCharacters(character_assignment(all_answers))
-        #session.setCharacters(None)
-    if session.wait_for_full_session():
+    if session.wait_for_session_ready():
         return json.dumps({"playerNames": session.playerNames, "character": session.getCharacter(id)})
     return abort('No other players found :(')
 
 
 @app.route('/stage/<id>', methods = ['POST'])
 def next_sub_stage(id):
-    challengeOutcomeLoc_dict = request.get_json()
-    # TODO Esteban Sarah here your function :)
-    # story = getStageStory(id, challengeOutcomeLoc_dict['challengeOutcome']
-    # TODO Esteban Sarah, please tell me how can I get information which two people are paired!
-    # place, challenge = new_place(player1, player2, places_in)
-    #
+    session.challengeOutcome[id] = request.get_json()
 
-    return json.dumps({
-        "story": [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque urna dolor, ac pellentesque neque mollis vel. Etiam nec semper nulla. In hac habitasse platea dictumst. Proin fermentum quis urna sit amet porta. Nunc sed feugiat orci. Quisque leo magna, scelerisque et ipsum vitae, volutpat egestas sapien. Quisque metus enim, vestibulum nec viverra gravida, elementum ut dolor.',
-            'Etiam efficitur arcu vitae enim tristique fermentum. Donec sit amet cursus ligula. Mauris facilisis sollicitudin cursus. Aenean eu lacus tortor. Ut dapibus, leo a molestie blandit, sapien eros vestibulum metus, sit amet suscipit felis nunc in augue. Aenean a rhoncus magna. Nullam elementum quam orci, nec mattis mauris dignissim non. Cras pellentesque eget tellus quis mattis. Nullam at leo a enim imperdiet bibendum. Donec volutpat dolor vel lectus molestie, ac tristique arcu varius. Vestibulum blandit maximus massa, eget blandit enim. Cras id ligula dolor. Cras eget sem sed est convallis vulputate. Curabitur consectetur nunc quam, vitae pharetra ipsum venenatis vel.'
-            ],
-        "destinationCoords": [48.149116,11.567532],
-        "destinationName": "TUM Stammgelände",
-        "challenge": {
-            "challenge": 'Was ist ein Wolpertinger?',
-            "challenge_type": 1,
-            "answers": [
-                'Ein Wolf',
-                'Ein Hase',
-                'Ein Vogel',
-                'Ein Fabelwesen'
-                ],
-                "right_answer": 'Ein Fabelwesen'
-        }
-   })
+    global session
+    if session.isReady():
+        # TODO call Esteban function
+        # session.playerNextChapter = ...
+        session.readyToPlay = True
+    if session.wait_for_session_ready():
+        return session.playerNextChapter[id]
+    return abort('MAX_TIMEOUT')
+#     return json.dumps({
+#         "story": [
+#             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque urna dolor, ac pellentesque neque mollis vel. Etiam nec semper nulla. In hac habitasse platea dictumst. Proin fermentum quis urna sit amet porta. Nunc sed feugiat orci. Quisque leo magna, scelerisque et ipsum vitae, volutpat egestas sapien. Quisque metus enim, vestibulum nec viverra gravida, elementum ut dolor.',
+#             'Etiam efficitur arcu vitae enim tristique fermentum. Donec sit amet cursus ligula. Mauris facilisis sollicitudin cursus. Aenean eu lacus tortor. Ut dapibus, leo a molestie blandit, sapien eros vestibulum metus, sit amet suscipit felis nunc in augue. Aenean a rhoncus magna. Nullam elementum quam orci, nec mattis mauris dignissim non. Cras pellentesque eget tellus quis mattis. Nullam at leo a enim imperdiet bibendum. Donec volutpat dolor vel lectus molestie, ac tristique arcu varius. Vestibulum blandit maximus massa, eget blandit enim. Cras id ligula dolor. Cras eget sem sed est convallis vulputate. Curabitur consectetur nunc quam, vitae pharetra ipsum venenatis vel.'
+#             ],
+#         "destinationCoords": [48.149116,11.567532],
+#         "destinationName": "TUM Stammgelände",
+#         "challenge": {
+#             "challenge": 'Was ist ein Wolpertinger?',
+#             "challenge_type": 1,
+#             "options": [
+#                 'Ein Wolf',
+#                 'Ein Hase',
+#                 'Ein Vogel',
+#                 'Ein Fabelwesen'
+#                 ],
+#                 "right_answer": 'Ein Fabelwesen'
+#         }
+#    })
 
 
 @app.route('/here/<id>')
