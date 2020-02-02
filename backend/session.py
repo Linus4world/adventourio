9191fbde6c55509bdf2005f5c087fb8b98e5e35c
Linus Kreitner <linus.kreitner@tum.de>
2020-02-02 14:32:31 +0100

    [B]refactor(session): bundle player requests

--- backend/session.py
+++ backend/session.py
@@ -5,9 +5,12 @@ class Session:
     playerIds = []
     playerNames = []
     playerAnswers = []
+    readyQueue = 0
     readyToPlay = False
     placesCategory = dict()
     playerCharacters = dict()
+    playerChallengeOutcome = dict()
+    playerNextChapter = dict()
 
     def __init__(self, maxPlayers = 4):
         self.maxPlayers = maxPlayers
@@ -19,10 +22,14 @@ class Session:
 
     def setCharacters(self, character_assignment):
         self.playerCharacters = character_assignment
+        self.readyQueue = self.maxPlayers
         self.readyToPlay = True
 
     def isFull(self) -> bool:
         return len(self.playerIds) == self.maxPlayers
+
+    def isReady(self) -> bool:
+        return self.readyQueue == self.maxPlayers - 1
 
     def getPlayerIndex(self, id: str): 
         return self.playerIds.index(id)
@@ -38,11 +45,18 @@ class Session:
     def getPlacesCategory(self):
         return self.placesCategory
 
-    def wait_for_full_session(self):
+    def wait_for_session_ready(self):
+        self.readyQueue += 1
         counter = 0
         while(counter < self.MAX_WAIT):
             if self.readyToPlay:
+                self.leave()
                 return True
             sleep(2)
             counter += 1
         return False
+
+    def leave(self):
+        self.readyQueue -= 1
+        if self.readyQueue == 0:
+            this.readyToPlay = False
